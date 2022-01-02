5c089776fa1169557758ee350170b35f6e35c9f8
Ghost Order <kodaichi.kun@gmail.com>
2022-01-02 17:05:11 -0500

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -8,3 +8,15 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const cartItem = cartItems.find(item => item.id === cartItemToRemove.id);
+  if (cartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+    ? { ...cartItem, quantity: cartItem.quantity -1 }
+    : cartItem
+  );
+}
