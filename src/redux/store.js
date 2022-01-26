ab1d691363c1ea893b1e3d5704509e046ce8cf2d
Ghost Order <kodaichi.kun@gmail.com>
2022-01-26 18:25:01 -0500

    moved our directory and shop data into its own reducer, created respective selctors and updated corresponding components with ux flow for directory/shop

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-const middlewares = [ logger ];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const middlewares = [ logger ];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
+export { store, persistor };
